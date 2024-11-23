8408e77ced19ffe316907826be12d6e7df324dcb
Zack Vera <qphys50@gmail.com>
2024-11-22 16:36:51 -0800

    Added all the files needed initially to update state and added the beginning of the formSlice logic;

--- src/App.js
+++ src/App.js
@@ -1,4 +1,17 @@
+import CarForm from "./components/CarForm";
+import CarList from "./components/CarList";
+import CarSearch from "./components/CarSearch";
+import CarValue from "./components/CarValue";
+
 function App() {
-  return <div>hi!</div>;
+  return (
+    <div>
+      <CarForm />
+      <CarList />
+      <CarSearch />
+      <CarValue />
+    </div>
+  );
 }
+
 export default App;
